68d5e1e6eacdac48efa7b47130eef7ef47d4d110
Melissa Holsapple <melissaholsapple@roadhouse.attlocal.net>
2016-07-20 21:15:42 -0400

    changes/updates to js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,131 @@
-$( document ).ready(function() {
+(function() {
+  var questions = [{
+    question1: "Who went back for more kisses in Snow White and the Seven Dwarfs?",
+    choices: [Grumpy, Sleepy, Dopey, Happy],
+    correctAnswer: Dopey
+  }, {
+    question2: "Who has large ears and flys?",
+    choices: [Goofy, Tinkerbell, Mickey, Dumbo],
+    correctAnswer: Dumbo
+  }, {
+    question3: "Who's real name is Eugene Fitzherbert?",
+    choices: [Genie, Flynn, Beast, Prince Eric],
+    correctAnswer: Flynn
+  }, {
+    question4: "Who has a lucky fin?",
+    choices: [Flounder, Dory, Ariel, Nemo],
+    correctAnswer: Nemo
+  }, {
+    question5: "Which princess lives with 7 small men?",
+    choices: [Snow White, Aurora, Belle, Cinderella],
+    correctAnswer: Snow White
+  }, {
+    question6: "Which of Pooh's friends has a special song about himself?",
+    choices: [Piglet, Eeyore, Tigger, Roo],
+    correctAnswer: Tigger
+  }, {
+    question7: "Which fairy wears all green?",
+    choices: [Rosetta, Tinkerbell, Fawn, Periwinkle],
+    correctAnswer: Tinkerbell
+  }, {
+    question8: "Which princess has talking household items?",
+    choices: [Jasmine, Pocahontas, Ariel, Belle],
+    correctAnswer: Belle
+  }, {
+    question9: "Who is Captain Hook's primary enemy?",
+    choices: [Peter Pan, Smee, Crocodile, Jake],
+    correctAnswer: Peter Pan
+  }, {
+    question10: "Which sidekick does the princess call a guppy?",
+    choices: [Nemo, Squirt, Flounder, Sebastian],
+    correctAnswer: Flounder
+  }];
+  
+  var game = {
 
-    function triviaGame () {
+    //Variables
+    questionTimeToGuess: 30,
+    questionNumber: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+    unAnswered: 0,
 
-      // Gets Link for Theme Song
-            var audioElement = document.createElement('audio');
-            audioElement.setAttribute('src', 'assets/rotten-to-the-core.m4a');
-            audioElement.play();
-      // Buttons to control the on click
-        $(".themeButton").on("click", function(){
-              audioElement.play();
-        });
+    //Functions
+    timer: function(){
 
-        $(".pauseButton").on("click", function(){
-              audioElement.pause();
-        });
+        // var number = 30;
+        // // $('#stop').on('click', stop);
+        // // $('#resume').on('click', run);
 
-    };
-      triviaGame();
-    });
+        // function run(){
+        //     counter = setInterval(decrement, 1000);
+        // }
+
+        // function decrement(){
+        //     number--;
+        //     $('#show-number').html('<h2>' + number + '</h2>');
+        //     if (number === 0){
+        //         stop();
+        //         alert('Time Up!')
+        //     }
+        // }
+
+        // function stop(){
+        //     clearInterval(counter);
+        // }
+
+        // run();
+
+
+    },
+
+    startGame: function(){
+    object.onclick=function(){myScript};
+    },
+
+    reset: function(){
+
+    },
+    checkGuess: function(){
+
+      questionNumber++
+      if (this.questionNumber === questionsArray.length) {
+      }
+    },
+
+    printQuestions: function() {
+      //reset timer
+      this.timer()
+
+      console.log(questionsArray[questionNumber].question1)
+      console.log(questionsArray[questionNumber].choices[0])
+      console.log(questionsArray[questionNumber].choices[1])
+      console.log(questionsArray[questionNumber].choices[2])
+      console.log(questionsArray[questionNumber].choices[3])
+      console.log(questionsArray[questionNumber].correctChoice)
+
+    }
+
+}
+
+
+  // $( document ).ready(function() {
+
+  //   function triviaGame () {
+
+  //     // Gets Link for Theme Song
+  //           var audioElement = document.createElement('audio');
+  //           audioElement.setAttribute('src', 'assets/music/rotten-to-the-core.m4a');
+  //           audioElement.play();
+  //     // Buttons to control the on click
+  //       $(".themeButton").on("click", function(){
+  //             audioElement.play();
+  //       });
+
+  //       $(".pauseButton").on("click", function(){
+  //             audioElement.pause();
+  //       });
+
+  //   };
+  //     triviaGame();
+})();
